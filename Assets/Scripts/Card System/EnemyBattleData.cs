51cb876bf9a7fbf8b9d7714c3766cbba06ce1127
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2020-10-16 16:11:26 +0300

    Update EnemyBattleData.cs
    added tooltips

--- Assets/Scripts/Card System/EnemyBattleData.cs
+++ Assets/Scripts/Card System/EnemyBattleData.cs
@@ -4,9 +4,16 @@
 public class EnemyBattleData : ScriptableObject
 {
     public string enemyName;
+    
     [Range(0, 200)] public int health;
     public Sprite enemyCard;
+
+    [Tooltip("How the enemy performs in battles.")]
     public EnemyBattleStyles battleStyle;
+    
+    [Tooltip("Beginner: Deck consists of only 45 Addition type cards." +
+            "\nIntermediate: Full deck of 152 cards, including Addition & Wild cards." +
+            "\nExpert: Currently same as Intermediate.")]
     public EnemyDifficulty difficulty;
 }
 
