d8c274512c74b144b0859763704cc0a84fa47da4
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2020-11-10 19:05:43 +0200

    Update GameManager.cs
    preparing for game manager to be used throughout the game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,24 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    #region Singleton
     public static GameManager gm;
+
     private void Awake()
     {
         if (gm == null)
             gm = this;
-    }
-    #endregion
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        DontDestroyOnLoad(gameObject);
     }
 }
