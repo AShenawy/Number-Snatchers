d9e4c7dd7ea62a562ef51b161dd550478f5e902c
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2020-11-10 20:36:30 +0200

    Main Menu linked to Game Screen
    player can now move from main menu screen to the game

---
+++ Assets/Scripts/Card System/MoveToScene.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class MoveToScene : MonoBehaviour
+{
+    public void GoScene(string name)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(name);
+    }
+}
