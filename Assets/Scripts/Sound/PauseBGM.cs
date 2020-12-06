7f607a005ba7e58c1bf20991ca8067b3af3c6fd3
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2020-12-06 23:19:17 +0200

    BGM pauses on win/lose messages

---
+++ Assets/Scripts/Sound/PauseBGM.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+
+// temporarily pauses BGM when instantiated. then resumes BGM when destroyed
+// this is mainly for flash and flow cards which pop up
+public class PauseBGM : MonoBehaviour
+{
+    private void Start()
+    {
+        SoundManager.instance.PauseBGM(true);
+    }
+
+    private void OnDestroy()
+    {
+        SoundManager.instance.PauseBGM(false);
+    }
+}
