fdbf9e42a560cadaf9ca6e56aead0ccb4ddfa189
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2020-12-07 22:35:20 +0200

    Keyboard can be used to input numbers and submit/pass challenge without mouse clicks

--- Assets/Scripts/Card System/ChallengeHandler.cs
+++ Assets/Scripts/Card System/ChallengeHandler.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class ChallengeHandler : MonoBehaviour
 {
@@ -11,12 +12,33 @@ public class ChallengeHandler : MonoBehaviour
     public Text challengeDetails;
     public int humanChallenge;
 
+
+    private void Start()
+    {
+        input.ActivateInputField();
+        input.Select();
+    }
+
     private void Update()
     {
         if (input.text == "")
             submitButton.interactable = false;
         else
             submitButton.interactable = true;
+
+        if (Input.GetButtonDown("Cancel"))
+        {
+            PassChallenge();
+            DestroyObject();
+            GetComponent<ButtonSFX>().PlayClickSFX();
+        }
+
+        if (Input.GetButtonDown("Submit") && submitButton.interactable == true)
+        {
+            Submit();
+            DestroyObject();
+            GetComponent<ButtonSFX>().PlayClickSFX();
+        }
     }
 
     public void Submit()
--- Assets/Scripts/Card System/GuessHandler.cs
+++ Assets/Scripts/Card System/GuessHandler.cs
@@ -8,12 +8,26 @@ public class GuessHandler : MonoBehaviour
     public Button submitButton;
     public int humanGuess;
 
+
+    private void Start()
+    {
+        input.ActivateInputField();
+        input.Select();
+    }
+
     private void Update()
     {
         if (input.text == "")
             submitButton.interactable = false;
         else
             submitButton.interactable = true;
+
+        if (Input.GetButtonDown("Submit") && submitButton.interactable == true)
+        {
+            Submit();
+            DestroyObject();
+            GetComponent<ButtonSFX>().PlayClickSFX();
+        }
     }
 
     public void Submit()
